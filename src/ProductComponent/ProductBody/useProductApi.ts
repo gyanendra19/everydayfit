f49e03e41bdab6535f3cb2937d993859c0378b0f
gyanendra19 <gyanendrav416@gmail.com>
2024-03-19 23:08:14 +0530

    changes

--- src/ProductComponent/ProductBody/useProductApi.ts
+++ src/ProductComponent/ProductBody/useProductApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const useProductApi = async(setAllProductResponse: (value: any) => void) => {
-    const res = await axios.get('/api/v1/product/getAllProduct')
+    const res = await axios.get('https://everydayfit-backend.onrender.com/api/v1/product/getAllProduct')
 
     setAllProductResponse(res.data.data)
 }
